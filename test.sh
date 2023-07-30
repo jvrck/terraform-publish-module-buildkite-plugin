02748a0a7b943fac5f354ff49085721a12a45b19
Jim Vrckovski <jim.vrckovski@gmail.com>
2023-07-30 17:02:41 +1000

    update command

--- test.sh
+++ test.sh
@@ -2,14 +2,22 @@
 
 set -eu
 
-TEST=[{\"github.com/jvrck/terraform-publish-module-buildkite-plugin#release-0.0.1\":null},{\"github.com/buildkite-plugins/docker-buildkite-plugin#v5.8.0\":{\"image\":\"alpine:latest\"}}]
+function format_url() {
+  local url=$1
+  url=${url//\//-}   # Replace '/' with '-'
+  url=${url//\./-}   # Replace '.' with '-'
+  url=${url//#/-}    # Replace '#' with '-'
+  echo $url
+}
 
+
+TEST=[{\"github.com/jvrck/terraform-publish-module-buildkite-plugin#release-0.0.1\":null},{\"github.com/buildkite-plugins/docker-buildkite-plugin#v5.8.0\":{\"image\":\"alpine:latest\"}}]
 echo "$TEST"
 
-IFS=','
-for item in "${TEST[@]}"; do
-  echo "Item: $item"
-  if [[ $item =~ 'image' ]]; then
-    echo "Image: ${item##*\"}:${item##*\"}"
-  fi
-done
+TFC_PLUGIN_URL=$(echo "$TEST" | jq -r '.[] | select(. | keys[] | contains("github.com/jvrck/terraform-publish-module-buildkite-plugin")) | keys[]')
+echo "$TFC_PLUGIN_URL"
+
+echo "==========="
+
+TFC_PLUGIN_PATH=$(format_url "$TFC_PLUGIN_URL")
+echo "$TFC_PLUGIN_PATH"
