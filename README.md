34255d14b8c7a5fbe206abb7a059890387ee4333
Jim Vrckovski <100883+jvrck@users.noreply.github.com>
2023-08-08 09:56:05 +1000

    0.0.1 (#1)
    * Initial release

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # terraform-publish-module-buildkite-plugin
+
+A Buildkite module to validate Terrafrom repositories. 
+
+The plugin will publish to Terraform cloud by tagging a branch. The plugin will append a `-dev` suffix to a branch that is not the main branch.
+
+To use the plugin, append the following to your buildkite pipeline.
+```
+steps:
+    # other steps...
+
+  - label: " :terraform: Deploy to Terraform Registry"
+    env:
+    plugins:
+      - jvrck/terraform-publish-module:
+```
---
+++ hooks/tag.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+: '
+This function checks if a given tag exists in a git repository.
+
+Usage:
+    check_git_tag /path/to/repository v1.0.0
+'
+function check_git_tag() {
+    repository_path="$1"
+    tag="$2"
+    
+    # Change directory to the repository path
+    cd "$repository_path" || exit
+    
+    # Fetch all tags from remote repository
+    git fetch --tags --quiet
+    
+    # Check if the given tag exists
+    if git rev-parse "$tag" >/dev/null 2>&1; then
+        echo "Tag '$tag' exists in the repository"
+        echo "Exiting with code 1"
+        exit 1
+    else
+        echo "Tag '$tag' does not exist in the repository. Create"
+    fi
+}
+
+# set default suffix
+SUFFIX="-dev"
+
+# Get the tag from the buildkite agent
+# This is the input from the user
+TAG=$(buildkite-agent meta-data get tag)
+
+# BUILDKITE_PIPELINE_DEFAULT_BRANCH - the main branch of the repo 
+# BUILDKITE_BRANCH - the branch that is being built
+
+# Ensure that non master / main branch tag contains suffix
+if [[ "${BUILDKITE_BRANCH}" != "${BUILDKITE_PIPELINE_DEFAULT_BRANCH}" ]]; then
+  if [[ "$TAG" == *"$SUFFIX"* ]]; then
+    echo "TAG contains SUFFIX"
+  else
+    echo "Adding SUFFIX to TAG"
+    TAG="$TAG$SUFFIX"
+  fi
+fi
+
+check_git_tag . "$TAG"
+
+git tag "$TAG"
+git push origin "$TAG"
